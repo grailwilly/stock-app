0c05dfa5febd06047f8ecd2a01e5187f516db22d
grailwilly <grailjwi11y@gmail.com>
2022-08-02 19:12:36 +0800

    Add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable
+         #:recoverable, :rememberable, :validatable
 end
